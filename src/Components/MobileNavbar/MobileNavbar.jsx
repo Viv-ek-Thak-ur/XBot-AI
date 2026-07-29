4232512d7c65a8dec2518d66bbe8bd52ac0cc04c
Viv-ek-Thak-ur <vivekchitti123@gmail.com>
2026-07-29 18:01:16 +0530

    DOM requrement Edition

--- src/Components/MobileNavbar/MobileNavbar.jsx
+++ src/Components/MobileNavbar/MobileNavbar.jsx
@@ -3,12 +3,12 @@ import Menu from "../../assets/menu.svg";
 
 export default function MobileNavbar(){
     return(
-        <header className={styles.mobileNavbar}>
+        <div className={styles.mobileNavbar}>
             <button type="button"className={styles.menuBtn}>
                 <img className={styles.menuIcon} src={Menu} alt="Menu" />
             </button>
             <h1 className={styles.navbarHeading}>Bot AI</h1>
-        </header>
+        </div>
 
     );
 }
