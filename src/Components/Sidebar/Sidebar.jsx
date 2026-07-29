cadd14490555588ef5f33ec3eb3c87bc8f6f5e4a
Viv-ek-Thak-ur <vivekchitti123@gmail.com>
2026-07-29 17:03:50 +0530

    Updated Sidebar -- Link for useNavigate

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,21 +1,24 @@
 import styles from "./Sidebar.module.css";
 import logo from "../../assets/logo.svg";
 import chatIcon from "../../assets/edit.svg";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Sidebar(){
-    const navigate = useNavigate();
+    
     return(
         <div className={styles.container}>
             <div className={styles.head}>
-                <img src={logo} alt="Logo" className={styles.icon} onClick={()=>(navigate("/"))}/>
+                <Link to="/">
+                <img src={logo} alt="Logo" className={styles.icon} />
+                </Link>
                 <p>New Chat</p>
-                <button type="submit" className={styles.chatBtn}>
-                    <img src={chatIcon} alt="Chat" className={styles.chatIcon} onClick={()=>(navigate("/"))}/>
-                </button>
+                <Link className={styles.chatBtn} to="/">
+                
+                    <img src={chatIcon} alt="Chat" className={styles.chatIcon}/>
+                </Link>
             </div>
             <div className={styles.history}>
-                <button type="submit" className={styles.historyBtn} onClick={()=>(navigate("/history"))}>Past Conversation</button>
+                <Link className={styles.historyBtn} to="/history">Past Conversation</Link>
             </div>
         </div>
     );
